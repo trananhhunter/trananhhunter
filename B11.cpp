e27472e0c1a4484a2ff0eb657bba2b3e9fcb18b6
trananhhunter <trananhhunter69@gmail.com>
2024-12-23 16:34:35 +0700

    File

--- B11.cpp
+++ B11.cpp
@@ -1,59 +1,170 @@
 #include <bits/stdc++.h>
+#include <fstream>
 using namespace std;
 
-using ll = long long;
-
-class Doanhnghiep{
-	private:
-		string ma,ten;
-		int soLuong;
+class Nguoi{
+	protected:
+		string hten;
+		int ns;
 	public:
-		void nhap();
-		void in();
-		int getSoluong(){
-			return this->soLuong;
+		Nguoi(){
+			hten = "";
+			ns =0;
 		}
-		string getMa(){
-			return this->ma;
+		void nhap(){
+			cout << "Ho ten : "; getline(cin,hten);
+			cout << "Nam sinh : "; cin >> ns;cin.ignore();
+		}
+		void in(){
+			cout << "Ho ten : " << hten << endl;
+			cout << "Nam sinh : " << setw(4)<< ns << endl;
+			
 		}
 };
 
+class GiangVien : public Nguoi{
+	protected:
+		string bomoncongtac,mondangday;
+		int sobaibao;
+	public:
+		GiangVien() : Nguoi(){
+		}
+		void nhapGiangVien(){
+			Nguoi::nhap();
+			cout << "Bo mon cong tac : "; getline(cin,bomoncongtac);
+			cout << "Mon dang giang day : "; getline(cin,mondangday);
+			cout << "So bai bao : "; cin >> sobaibao; cin.ignore();
+		}
+		void inGiangVien(){
+			Nguoi::in();
+			cout << "Bo mon cong tac : " << bomoncongtac << endl;
+			cout << "Mon dang giang day : " << mondangday << endl;
+			cout << "So bai bao : " << setw(3) << setfill('0') <<sobaibao << endl;
+		}
+		void ghiFile(fstream& file);
+		void docFile(fstream& file);
+		string getBoMonCongTac(){
+			return bomoncongtac;
+		}
+		int getSoBaiBao(){
+			return sobaibao;
+		}
+};
 
-void Doanhnghiep::nhap(){
-	cin >> ma;
-	cin.ignore();
-	getline(cin,ten);
-	cin >> soLuong;
+void GiangVien::ghiFile(fstream& file){
+	file << hten << endl << ns << endl << bomoncongtac << endl << mondangday << endl << sobaibao << endl;
 }
 
-void Doanhnghiep::in(){
-	cout << ma << " " << ten << " " << soLuong << endl;
+void GiangVien::docFile(fstream& file){
+	getline(file,hten);
+	file >> ns; file.ignore();
+	getline(file,bomoncongtac);
+	getline(file,mondangday);
+	file >> sobaibao; file.ignore();
+}
+
+void nhapDsGiangVien(vector<GiangVien>& v,int n){
+	for(int i=0;i<n;i++){
+		cout << "Nhap giang vien thu " << i + 1 << ":\n";
+		GiangVien a;
+		a.nhapGiangVien();
+		v.push_back(a);
+	}
+	cout << "Da nhap thanh cong " << n << " giang vien !\n";
+}
+
+void inDsGiangVien(vector<GiangVien>& v){
+	cout << "Danh sach giang vien :\n";
+	for(GiangVien& x : v){
+		x.inGiangVien();
+		cout << "-------------\n";
+	}
 }
 
 
-bool cmp(Doanhnghiep a,Doanhnghiep b){
-	if(a.getSoluong() != b.getSoluong())
-		return a.getSoluong() > b.getSoluong();
-	return a.getMa() < b.getMa();
+void ghiDsVaoFile(vector<GiangVien>& v, string& tenfile){
+	fstream f(tenfile,ios::out | ios::app);
+	if(!f){
+		cerr << "Khong the mo file ghi \n";
+		return;
+	}
+	 for(auto& gv : v) {
+        gv.ghiFile(f);
+    }
+	f.close();
+	cout << "Da ghi ds vao file : " << tenfile << endl;
+}
+
+
+void docDsTuFile(vector<GiangVien>& v, string& tenfile){
+	fstream f(tenfile,ios::in);
+	if (!f) {
+        cerr << "Khong the mo file ghi \n";
+        return;
+    }
+     GiangVien gv;
+    while (!f.eof()) {
+       
+        gv.docFile(f);
+        if (f.fail()) break; 
+        v.push_back(gv);
+    }
+	f.close();
+}
+
+bool boMonCongTac(GiangVien &a,GiangVien &b){
+	return a.getBoMonCongTac() < b.getBoMonCongTac();
+}
+
+bool soBaiBao(GiangVien &a,GiangVien &b){
+	return a.getSoBaiBao() > b.getSoBaiBao();
+}
+
+void sxBoMon(vector<GiangVien>& v){
+	sort(v.begin(),v.end(),boMonCongTac);
+	cout << "Danh sach giang vien theo tung bo mon :\n";
+	inDsGiangVien(v);
+}
+
+void sxBaiBao(vector<GiangVien>& v){
+	sort(v.begin(),v.end(),soBaiBao);
+	cout << "Danh sach giang vien giam dan theo so bai bao :\n";
+	inDsGiangVien(v);
 }
 
 int main(){
-	int n; cin >> n;
-	Doanhnghiep a[1000];
-	for(int i=0;i<n;i++){
-		a[i].nhap();
-	}
-	sort(a,a+n,cmp);
-	int q; cin >> q;
-	while(q--){
-		int x,y; cin >> x >> y;
-		cout << "Danh sach doanh nghiep nhan tu " << x << "den " << y << "SINH VIEN" << endl;
-		for(auto ok : a){
-			if(ok.getSoluong() >=x && ok.getSoluong() <= y){
-				ok.in();
-			}
-		}
-	}
-	return 0;
+	vector<GiangVien> ds;
+	int n;
+	string tenfile = "C:\\Users\\user\\OneDrive\\Máy tính\\FILE CODE\\DeCuong\\TXT\\DanhsachGiangVien";
+	cout << "Nhap so luong giang vien : "; cin >> n; cin.ignore();
+	nhapDsGiangVien(ds,n);
+	ghiDsVaoFile(ds,tenfile);
+	
+	vector<GiangVien> doctuFile;
+	docDsTuFile(doctuFile,tenfile);
+	inDsGiangVien(doctuFile);
+	
+	cout << "------------------\n";
+	sxBoMon(doctuFile);
+	cout << "------------------\n";
+	sxBaiBao(doctuFile);
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
--- B12.cpp
+++ B12.cpp
@@ -1,41 +1,244 @@
-#include <bits/stdc++.h>
+#include <iostream>
+#include <fstream>
+#include <iomanip>
+#include <vector>
+#include <string>
 using namespace std;
 
-class Nhanvien{
-	private:
-		string ma,ten,gt,ns,dc,mst,hd;
-	public:
-		Nhanvien();
-		friend istream& operator >> (istream&,Nhanvien&);
-		friend ostream& operator << (ostream&,Nhanvien);
+class SinhVien {
+protected:
+    string hoten;
+    int ns;
+    double d[5], dtb;
+
+public:
+    SinhVien() {}
+
+    double tinhDTB() {
+        double sum = 0;
+        for (int i = 0; i < 5; i++) {
+            sum += d[i];
+        }
+        return sum / 5;
+    }
+
+    void nhap() {
+        cin.ignore(); // B? dòng th?a tru?c khi nh?p chu?i
+        cout << "Ho ten: ";
+        getline(cin, hoten);
+        cout << "Nam sinh: ";
+        cin >> ns;
+        for (int i = 0; i < 5; i++) {
+            cout << "Diem thu " << i + 1 << ": ";
+            cin >> d[i];
+        }
+        dtb = tinhDTB();
+    }
+
+    void in() const {
+        cout << "Ho ten: " << hoten << "\tNam sinh: " << ns << endl;
+        for (int i = 0; i < 5; i++) {
+            cout << fixed << setprecision(1) << d[i] << "\t";
+        }
+        cout << endl;
+        cout << "DTB: " << fixed << setprecision(1) << dtb << endl;
+    }
+
+    void ghiFile(fstream &file) const {
+        file << hoten << "\n" << ns << "\n";
+        for (int i = 0; i < 5; i++) {
+            file << d[i] << " ";
+        }
+        file << "\n" << dtb << "\n";
+    }
+
+    void docFile(fstream &file) {
+        getline(file, hoten);
+        if (file.eof()) return;
+        file >> ns;
+        for (int i = 0; i < 5; i++) {
+            file >> d[i];
+        }
+        file >> dtb;
+        file.ignore(); // B? dòng th?a
+    }
 };
 
-Nhanvien::Nhanvien(){
-	ma = ten = gt = ns = dc = mst = hd = "";
+double gpa(double d) {
+    if (d >= 9.0) return 4.0;
+    else if (d >= 8.5) return 3.7;
+    else if (d >= 8.0) return 3.5;
+    else if (d >= 7.0) return 3.0;
+    else if (d >= 6.5) return 2.5;
+    else if (d >= 5.5) return 2.0;
+    else if (d >= 5.0) return 1.5;
+    else if (d >= 4.0) return 1.0;
+    else return 0.0;
 }
 
-istream& operator >> (istream& in,Nhanvien& a){
-	a.ma = "00001";
-	cout << "Ten : "; in.ignore();
-	getline(in,a.ten);
-	cout << "Gioi tinh : "; in >> a.gt;
-	cout << "Ngay sinh : "; in >> a.ns;
-	cin.ignore();
-	cout << "Dia chi : "; getline(in,a.dc);
-	cout << "Ma so thuc : "; in >> a.mst;
-	cout << "Ngay ki hop dong : "; in >> a.hd;
-	return in;
+void nhapSinhVien(vector<SinhVien> &v) {
+    int n;
+    cout << "Nhap so luong sinh vien: ";
+    cin >> n;
+    for (int i = 0; i < n; i++) {
+        cout << "Sinh vien thu " << i + 1 << ":\n";
+        SinhVien a;
+        a.nhap();
+        v.push_back(a);
+    }
+    cout << "Da nhap thanh cong " << n << " sinh vien.\n";
 }
 
-ostream& operator << (ostream& out,Nhanvien a){
-	out << a.ma << " " << a.ten << " " << a.gt << " " << a.ns << " " << a.dc << " " << a.mst << " " << a.hd << endl;
-	return out;
+void inSinhVien(const vector<SinhVien> &v) {
+    cout << "Danh sach sinh vien:\n";
+    for (const SinhVien &x : v) {
+        x.in();
+        cout << "-------OK-------\n";
+    }
 }
 
-#define a() a
-
-int main(){
-	Nhanvien a();
-	cin >> a;
-	cout << a;
+void ghiDsVaoFile(const vector<SinhVien> &v, const string &tenfile) {
+    fstream f(tenfile, ios::out);
+    if (!f) {
+        cerr << "Khong mo duoc file.\n";
+        return;
+    }
+    for (const SinhVien &x : v) {
+        x.ghiFile(f);
+    }
+    f.close();
+    cout << "Da ghi du lieu vao file " << tenfile << endl;
 }
+
+void docDsRaFile(vector<SinhVien> &v, const string &tenfile) {
+    fstream f(tenfile, ios::in);
+    if (!f) {
+        cerr << "Khong mo duoc file.\n";
+        return;
+    }
+    v.clear();
+    while (!f.eof()) {
+        SinhVien a;
+        a.docFile(f);
+        if (f.fail()) break;
+        v.push_back(a);
+    }
+    f.close();
+}
+
+void ThiLai(const vector<SinhVien> &v) {
+    cout << "Danh sach sinh vien thi lai:\n";
+    int cnt = 0;
+    for (const SinhVien &x : v) {
+        bool thiLai = false;
+        for (int i = 0; i < 5; i++) {
+            if (x.d[i] < 5) {
+                thiLai = true;
+                break;
+            }
+        }
+        if (thiLai) {
+            x.in();
+            cout << "-------OK-------\n";
+            cnt++;
+        }
+    }
+    if (cnt == 0) cout << "Khong co sinh vien nao thi lai.\n";
+}
+
+void thiTotNghiep(const vector<SinhVien> &v) {
+    cout << "Danh sach sinh vien thi tot nghiep:\n";
+    int cnt = 0;
+    for (const SinhVien &x : v) {
+        bool thiTotNghiep = true;
+        for (int i = 0; i < 5; i++) {
+            if (x.d[i] < 5) {
+                thiTotNghiep = false;
+                break;
+            }
+        }
+        if (thiTotNghiep && x.tinhDTB() < 7) {
+            x.in();
+            cout << "-------OK-------\n";
+            cnt++;
+        }
+    }
+    if (cnt == 0) cout << "Khong co sinh vien nao thi tot nghiep.\n";
+}
+
+void doAnTN(const vector<SinhVien> &v) {
+    cout << "Danh sach sinh vien lam do an tot nghiep:\n";
+    int cnt = 0;
+    for (const SinhVien &x : v) {
+        bool lamDATN = true;
+        for (int i = 0; i < 5; i++) {
+            if (x.d[i] < 5) {
+                lamDATN = false;
+                break;
+            }
+        }
+        if (lamDATN && x.tinhDTB() >= 7) {
+            x.in();
+            cout << "-------OK-------\n";
+            cnt++;
+        }
+    }
+    if (cnt == 0) cout << "Khong co sinh vien nao lam do an tot nghiep.\n";
+}
+
+void gpaSinhVien(const SinhVien &a) {
+    a.in();
+    cout << "Thang diem 4:\n";
+    for (int i = 0; i < 5; i++) {
+        cout << gpa(a.d[i]) << " ";
+    }
+    cout << endl;
+}
+
+void GpaDsSV(const vector<SinhVien> &v) {
+    cout << "Danh sach sinh vien:\n";
+    for (const SinhVien &x : v) {
+        gpaSinhVien(x);
+        cout << "-------OK-------\n";
+    }
+}
+
+int main() {
+    vector<SinhVien> v;
+    string tenfile = "danhsach.txt";
+
+    while (true) {
+        cout << "----- Menu ------\n";
+        cout << "1. Nhap n sinh vien va ghi vao file\n";
+        cout << "2. Hien thi danh sach sinh vien doc tu file\n";
+        cout << "3. Danh sach sv thi lai\n";
+        cout << "4. Danh sach sv thi tot nghiep\n";
+        cout << "5. Danh sach sv lam do an tot nghiep\n";
+        cout << "6. Quy doi diem sang thang diem 4\n";
+        cout << "0. Thoat\n";
+        cout << "---> Nhap lua chon: ";
+        int lc;
+        cin >> lc;
+        if (lc == 1) {
+            nhapSinhVien(v);
+            ghiDsVaoFile(v, tenfile);
+        } else if (lc == 2) {
+            docDsRaFile(v, tenfile);
+            inSinhVien(v);
+        } else if (lc == 3) {
+            ThiLai(v);
+        } else if (lc == 4) {
+            thiTotNghiep(v);
+        } else if (lc == 5) {
+            doAnTN(v);
+        } else if (lc == 6) {
+            GpaDsSV(v);
+        } else if (lc == 0) {
+            break;
+        } else {
+            cout << "Lua chon khong hop le!\n";
+        }
+    }
+    return 0;
+}
+
